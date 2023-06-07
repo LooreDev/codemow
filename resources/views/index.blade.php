a2b878fe7369c37072f8d0a83fa7d687bccd08ce
Pedro de Jesus Hernandez Palacios <jesussnake9@gmail.com>
2023-06-07 14:41:26 -0600

    CambiosAgradagosJesus

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Jesus</title>
 </head>
 <body>
+
     @include('layout.navbar')
     <h1>Index</h1>
     @include('layout.footer')
